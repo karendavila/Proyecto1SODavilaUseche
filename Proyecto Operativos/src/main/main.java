d57368df2ba64e3e3b9f7f71d444e76df830b262
MadDog159 <77283910+MadDog159@users.noreply.github.com>
2023-02-19 01:02:42 -0400

    Add files via upload

---
+++ Proyecto Operativos/src/main/main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import interfaz.casaRodaje;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author epidu
+ */
+public class main {
+
+           /**
+            * @param args the command line arguments
+            */
+           public static void main(String[] args) {
+                      // TODO code application logic here
+                      Scanner read = new Scanner(System.in);
+                      ProductorIntro hola = new ProductorIntro();
+                     // hola.start();
+                 //     hola.introCreada();
+                      
+                      casaRodaje inicial = new casaRodaje();
+                      inicial.setVisible(true);
+                      
+                       
+           }
+           
+}
